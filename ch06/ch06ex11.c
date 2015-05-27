3d1bd4fe8d5ba535288565387612a0fcd104d861
mentatmm <monsieur.mattingly@gmail.com>
2015-05-27 11:31:42 +0200

    ch06ex11
    Initial commit for ch06ex11.

---
+++ ch06/ch06ex11.c
@@ -0,0 +1,27 @@
+// ch06ex11.c
+
+// Write a program that reads eight integers into an array & then prints them in reverse order.
+
+#include <stdio.h>
+
+int main(void)
+{
+    const int SIZE = 8;
+    int number[SIZE];
+    printf("Enter 8 integer values separated by spaces: ");
+
+    int i;
+    
+    for (i = 0; i < SIZE; i++)
+    {
+        scanf("%d", &number[i]);
+    }
+
+    for (i = SIZE - 1; i >= 0; i--)
+    {
+        printf("%d ", number[i]);
+    }
+
+	getch();
+    return 0;
+}
